696d7041635cc063dbbc93c4ebdf6caf5253fbf6
guyzo0 <nembouetmerlin@gmail.com>
2023-05-17 21:30:18 +0100

    changes hello for v2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
         <p>
           My first <code>Amplify</code> React App.
         </p>
